68c8627f8c6b10c4a13fb6781c1dbd6f93eabb58
Ashes023! <ashaharyunuskhan@gmail.com>
2024-03-15 05:05:08 +0530

    Made basic calci

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-let prevnum = 0;
-let newnum = 0;
+let prevnum = NaN;
+let newnum = NaN;
 let oper = "";
+let check = false;
+
+let dis = document.querySelector(".display");
 
 let press = document.querySelectorAll(".number");
 press.forEach(function (button) {
@@ -9,13 +12,68 @@ press.forEach(function (button) {
   });
 });
 
-function display(num) {
-  let dis = document.querySelector(".display");
-  update_number();
-  dis.innerHTML = num;
+let ac = document.querySelector(".clear");
+ac.addEventListener("click", () => {
+  clear();
+});
+
+let operator = document.querySelectorAll(".operator");
+operator.forEach((button) => {
+  button.addEventListener("click", () => {
+    if (oper == "") {
+      oper = button.value;
+      prevnum = newnum;
+      newnum = NaN;
+      display("");
+    }
+    console.log(oper);
+  });
+});
+
+let equal = document.querySelector(".equal");
+equal.addEventListener("click", () => {
+  if (!isNaN(newnum)) {
+    check = true;
+    display(calculate(prevnum, oper, newnum));
+    initial();
+  }
+});
+
+function initial() {
+  prevnum = NaN;
+  newnum = NaN;
+  oper = "";
 }
 
-function operate(numb1, operator, numb2) {
+function clear() {
+  initial();
+  display("");
+}
+
+function display(num) {
+  if (!check) {
+    num = update_number(num);
+  }
+  dis.innerHTML = num;
+  check = false;
+}
+
+function update_number(numb) {
+  if (isNaN(newnum)) {
+    console.log(10);
+    newnum = numb;
+    return newnum;
+  }
+  if (newnum > 9999999) {
+    return newnum;
+  }
+  newnum = newnum + numb;
+  return newnum;
+}
+
+function calculate(numb1, operator, numb2) {
+  numb1 = parseInt(numb1);
+  numb2 = parseInt(numb2);
   if (operator == "+") {
     return numb1 + numb2;
   } else if (operator == "-") {
@@ -23,6 +81,9 @@ function operate(numb1, operator, numb2) {
   } else if (operator == "*") {
     return numb1 * numb2;
   } else if (operator == "/") {
+    if(numb2==0){
+        return 'WTF!'
+    }
     return numb1 / numb2;
   }
 }
